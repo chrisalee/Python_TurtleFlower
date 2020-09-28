0a874415e6355db90f5aa126283249be87344ee6
Christopher L <56335217+chrisalee@users.noreply.github.com>
2020-09-28 14:24:11 -0500

    Add files via upload

---
+++ TurtleFlower/main.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+speed(15)
+hideturtle()
+color('black', 'orange')
+
+def left_turn():
+    for i in range(10):
+        forward(15)
+        left(9)
+        
+def petal():
+    begin_fill()
+    left_turn()
+    left(90)
+    left_turn()
+    left(90)
+    end_fill()
+
+for i in range(5):
+    petal()
+    right(360/5)
+
+bye()
